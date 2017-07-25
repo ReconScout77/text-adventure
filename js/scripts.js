874e3b7be984f1a3d530b81d35c067ceb67bc06b
Jesse Bryan <jesse.bryan22@gmail.com>
2017-07-25 13:38:43 -0700

    Add specification 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
 //Business logic
 
 function Room () {
-  this.
+
+}
+
+var showHelp = function() {
+  $(".mainSection").append(
+    "<p>Commands for House Escape:</p>" +
+    "<p>look = look around the room</p>" +
+    "<p>open + object = attempt to open the object</p>" +
+    "<p>use + object = interact with the object in the room or inventory</p>" +
+    "<p>grab + object = attempt to grab the object</p>" +
+    "<p>inspect + object = obtain further details about the object</p>"
+    )
 }
 
 //User Interface Logic
-$(document).ready(function() [
-  event.preventDefault();
-]
+$(document).ready(function() {
+  $("#formInput").submit(function(event){
+    event.preventDefault();
+    var userText = $("#inputArea").val()
+    switch(userText) {
+      case "help":
+        showHelp();
+    };
+  });
+});
